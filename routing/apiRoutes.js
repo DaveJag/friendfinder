f0a63bb13af0ac88faf03b30eb7360cad173f6ab
DaveJag <davejag@comcast.net>
2017-09-19 22:31:28 -0400

    Still not bringing CSS styles into html pages, but... whatever.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,4 +1,4 @@
-//Dave Jagodowski - apiRoutes.js
+//Dave Jagodowski - apiRoutes.j
 //Require the friends.js data file
 var friends = require("../data/friends");
 var bestMatch = ""; //object to hold data of best match
@@ -34,13 +34,13 @@ module.exports = function(app){
 		var userScores = userData.scores;
 
 		// Loop through all the friend possibilities in the database. 
-		for  (var i=0; i<friends.length; i++) {
+		for  (var i=0; i < friends.length; i++) {
 
 			console.log(friends[i].name);
 			totalDifference = 0;
 
 			// Loop through all the scores of each friend
-			for (var j=0; j<friends[i].scores[j]; j++){
+			for (var j=0; j < friends[i].scores[j]; j++){
 
 				// Find absolute value of difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
